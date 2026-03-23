46a857a8248e542217839170efca774fb38468a8
millerz50 <135332424+millerz50@users.noreply.github.com>
2026-03-23 22:19:09 +0200

    Update next.config.ts to imply images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* other config options here */
+  images: {
+    domains: [
+      "source.unsplash.com", // for random laptop images
+      "example.com", // replace with your own image host if any
+    ],
+  },
 };
 
 export default nextConfig;
