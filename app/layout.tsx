88a870d54c374d32cf2bab0f90b42bc03f28e5b4
millerz50 <135332424+millerz50@users.noreply.github.com>
2026-03-24 00:29:00 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import  footer from "@/components/Footer"
 
 export const metadata = {
   title: "Tymo Electronics & Perfume Store | Darwin",
@@ -35,17 +36,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <main className="min-h-screen">{children}</main>
 
         {/* Footer */}
-        <footer className="w-full bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-gray-200 py-6 mt-12 border-t border-gray-200 dark:border-gray-700">
-          <div className="max-w-7xl mx-auto px-4 text-center">
-            <p className="text-sm">
-              &copy; {new Date().getFullYear()} Tymo Store Darwin. All rights
-              reserved.
-            </p>
-            <p className="text-sm mt-1">
-              Electronics, Laptop Sales, Repairs & Luxury Perfumes.
-            </p>
-          </div>
-        </footer>
+       <footer/>
       </body>
     </html>
   );
